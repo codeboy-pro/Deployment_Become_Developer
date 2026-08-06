361ac7ef6390add7e40719193348f18140175292
Pradip Maity <pradipmaity9907@gmail.com>
2026-08-06 15:29:11 +0530

    Add docker-aws as a normal folder

---
+++ docker-aws/Backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+import { YSocketIO } from "y-socket.io/dist/server";
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const ySocketIO = new YSocketIO(io);
+ySocketIO.initialize();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is running",
+    success: true,
+  });
+});
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    message: "Server is healthy",
+    success: true,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ docker-aws/Frontend/src/app/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css"
+import { Editor } from "@monaco-editor/react"
+import { MonacoBinding } from "y-monaco"
+import { useRef, useMemo } from "react"
+import * as Y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
+
+function App() {
+
+  const editorRef = useRef(null)
+
+  const ydoc = useMemo(() => new Y.Doc(), [])
+  const yText = useMemo(() => ydoc.getText("monaco"), [ ydoc ])
+
+
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc,{
+  autoConnect: true,
+})
+    new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([ editorRef.current ]),
+      provider.awareness
+    )
+  }
+  return (
+    <main
+      className="h-screen w-full bg-gray-950 flex gap-4 p-4"
+    >
+      <aside
+        className="h-full w-1/4 bg-amber-50 rounded-lg "
+      >
+        <h2 className="text-2xl font-bold p-4 border-b border-gray-300">Users</h2>
+        
+
+      </aside>
+      <section
+        className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          theme="vs-dark"
+          onMount={handleMount}
+        />
+      </section>
+
+    </main>
+  )
+}
+
+export default App
